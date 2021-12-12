3d290a257c0af0c4eb427b63946652fdab2acd67
Guilherme Rigotti <ggrigotti@gmail.com>
2021-12-11 23:36:50 -0300

    feat: browsing standard library documentation

---
+++ src/bin/a17.rs
@@ -0,0 +1,9 @@
+// Topic: Browsing standard library documentation
+
+fn main() {
+    let uppercase = "hello";
+    let lowercase = "HELLO";
+
+    println!("uppercase {:?}", uppercase.to_uppercase());
+    println!("lowercase {:?}", lowercase.to_lowercase());
+}
